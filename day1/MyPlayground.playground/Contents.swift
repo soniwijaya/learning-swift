82a3f2e838ebf399c2181d4d0eb11fc976e46499
Soni Wijaya <soni@macbook-pro.local>
2019-05-31 23:03:00 +0700

    day 1 of 100 days

---
+++ day1/MyPlayground.playground/Contents.swift
@@ -0,0 +1,34 @@
+// Variable Test
+import UIKit
+
+var str = "Hello, Swift Language"
+str = "Good Bye."
+
+// String and Integers
+var age = 38
+var population = 8_000_000
+
+// Multiline String
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+// Doubles and Booleans
+var pi = 3.141
+var awesome = true
+
+// String interpolation
+var score = 85
+var str3 = "Your score was \(score)"
+var results = "The test result as here: \(str3)"
+
+// Constants
+let taylor = "swift"
+
+// Type annotations
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
