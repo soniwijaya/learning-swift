0ec483c85608ef7f507e7c3f9ae882f848b6cd2c
Soni Wijaya <soni@macbook-pro.local>
2019-06-18 23:17:23 +0700

    day 19 of 100 days

---
+++ day19/Project2/Project2/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Soni Wijaya on 18/06/2019.
+//  Copyright © 2019 Soni Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 2
+        button3.layer.borderWidth = 3
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor(red: 1.0, green: 0.6, blue: 0.3, alpha: 1.0).cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
+    }
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+
+
+}
+
